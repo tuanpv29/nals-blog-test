6c9caab2276c0c9259bd7461942ae8a83d874a2d
tuanphan <tuanpv29@gmail.com>
2024-04-06 12:53:10 +0700

    feat: detail page UI

--- src/app/pages/blog-detail/blog-detail.component.ts
+++ src/app/pages/blog-detail/blog-detail.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-blog-detail',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './blog-detail.component.html',
   styleUrl: './blog-detail.component.scss',
 })
